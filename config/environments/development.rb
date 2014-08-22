b45b75d481e87e59cdf2724474592289d120737a
nurulshuvo <shuvo@e-solutionpark.com>
2014-08-22 19:01:13 +0600

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,8 +53,8 @@ Prelaunchr::Application.configure do
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :domain               => 'gmail.com',
-      :user_name            => 'shuvo@e-solutionpark.com',
-      :password             => 'shuvo123',
+      :user_name            => '',
+      :password             => '',
       :authentication       => 'plain',
       :enable_starttls_auto => true
   }
